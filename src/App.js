d7e65212cb95c1bbeee77fba41663d9b6c22cfd4
krish0ck <abdulla.nur.faisal66@gmail.com>
2022-01-25 12:31:25 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useState} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const off = "images/bulbOff.gif";
+    const on = "images/bulbOn.gif";
+    const [state, setState] = useState(off);
+    return (
+        <>
+            <img 
+                onClick={() => {
+                    if (state == off) {
+                        setState(on);
+                    }
+                    else {
+                        setState(off);
+                    }
+                 }} 
+                 src={state} 
+                 id="bulb"
+            />
+        </>
+    )
 }
-
 export default App;
